b69c0105dbf59a9513bde82564506e7b9ef4e7c2
kristyldatical <kgomes@datical.com>
2020-05-25 09:50:22 -0500

    updates

--- src/main/java/com/liquibase/testharness/DemoApplication.java
+++ src/main/java/com/liquibase/testharness/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.liquibase.testharness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/liquibase/testharness/DemoApplicationTests.java
+++ src/test/java/com/liquibase/testharness/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.liquibase.testharness;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
